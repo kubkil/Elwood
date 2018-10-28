b5edef0491b347b733a2e7d90b0d170f2165de8f
Jakub Kilar <kuba.kilar@gmail.com>
2018-10-28 15:38:05 +0100

    correct classes names in window click listener

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ const navbarToggle = document.querySelector('.navbar__toggle');
 const expandMenu = () => {
   navbarToggle.checked = true;
   hamburger.setAttribute('aria-expanded', 'true');
-}
+};
 
 const hideMenu = () => {
   navbarToggle.checked = false;
@@ -28,8 +28,8 @@ hamburger.addEventListener('click', () => {
 });
 
 window.addEventListener('click', (e) => {
-  const hamburgerBox = document.querySelector('.hamburger-box');
-  const hamburgerInner = document.querySelector('.hamburger-inner');
+  const hamburgerBox = document.querySelector('.navbar__hamburger-box');
+  const hamburgerInner = document.querySelector('.navbar__hamburger-inner');
 
   if (e.target !== hamburger && e.target !== hamburgerBox && e.target !== hamburgerInner) {
       hamburger.classList.remove('is-active');
