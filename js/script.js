716d593f87903666dad64639821d46597f70a4d3
Jakub Kilar <kuba.kilar@gmail.com>
2018-11-03 11:16:14 +0100

    Remove unnecessary toggle

--- js/script.js
+++ js/script.js
@@ -1,43 +1,42 @@
 'use strict';
 
 if (window.screen.width < 768) {
-const hamburger = document.querySelector('.navbar__hamburger');
-const navbarToggle = document.querySelector('.navbar__toggle');
+  const hamburger = document.querySelector('.navbar__hamburger');
+  const navbarToggle = document.querySelector('.navbar__toggle');
   const navbarAndMenu = [document.querySelector('.navbar'), document.querySelector('.navbar__list')];
 
-const expandMenu = () => {
-  navbarToggle.checked = true;
-  hamburger.setAttribute('aria-expanded', 'true');
+  const expandMenu = () => {
+    navbarToggle.checked = true;
+    hamburger.setAttribute('aria-expanded', 'true');
     [...navbarAndMenu].forEach(elem => elem.classList.add('navbar__background-js'));
-};
+  };
 
-const hideMenu = () => {
-  navbarToggle.checked = false;
-  hamburger.setAttribute('aria-expanded', 'false');
+  const hideMenu = () => {
+    navbarToggle.checked = false;
+    hamburger.setAttribute('aria-expanded', 'false');
     [...navbarAndMenu].forEach(elem => elem.classList.remove('navbar__background-js'));
-};
+  };
 
-const toggleClass = () => {
-  hamburger.classList.toggle('is-active');
-  homeSection.classList.toggle('home__blurred-js');
-  if (hamburger.classList.contains('is-active')) {
-    expandMenu();
-  } else {
-    hideMenu();
-  }
-};
+  const toggleClass = () => {
+    hamburger.classList.toggle('is-active');
+    if (hamburger.classList.contains('is-active')) {
+      expandMenu();
+    } else {
+      hideMenu();
+    }
+  };
 
-hamburger.addEventListener('click', () => {
-  toggleClass();
-});
+  hamburger.addEventListener('click', () => {
+    toggleClass();
+  });
 
-window.addEventListener('click', (e) => {
-  const hamburgerBox = document.querySelector('.navbar__hamburger-box');
-  const hamburgerInner = document.querySelector('.navbar__hamburger-inner');
+  window.addEventListener('click', (e) => {
+    const hamburgerBox = document.querySelector('.navbar__hamburger-box');
+    const hamburgerInner = document.querySelector('.navbar__hamburger-inner');
 
-  if (e.target !== hamburger && e.target !== hamburgerBox && e.target !== hamburgerInner) {
-    hamburger.classList.remove('is-active');
-    hideMenu();
-  }
-});
+    if (e.target !== hamburger && e.target !== hamburgerBox && e.target !== hamburgerInner) {
+      hamburger.classList.remove('is-active');
+      hideMenu();
+    }
+  });
 }
