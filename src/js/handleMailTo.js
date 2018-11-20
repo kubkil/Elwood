2fd35e7ab5a1877534ed0f3616606a1bfb059578
Jakub Kilar <kuba.kilar@gmail.com>
2018-11-20 10:21:16 +0100

    Add module
    Function handles decryption of an email address using ROT47 as antispam
    measure.

---
+++ src/js/handleMailTo.js
@@ -0,0 +1,24 @@
+const handleMailTo = (() => {
+  const mailLink = document.querySelector('.footer__link--gmail');
+  const decode = mail => {
+    const decoded = [];
+    for (let i = 0; i < mail.length; i += 1) {
+      const j = mail.charCodeAt(i);
+      if (j >= 33 && j <= 126) {
+        decoded[i] = String.fromCharCode(33 + ((j + 14) % 94));
+      } else {
+        decoded[i] = String.fromCharCode(j);
+      }
+    }
+    return decoded.join('');
+  };
+
+  const showMail = () => {
+    const mailAddress = decode('6=H@@54C27EDo8>2:=]4@>');
+    mailLink.setAttribute('href', `mailto:${mailAddress}`);
+  };
+
+  mailLink.addEventListener('click', showMail);
+})();
+
+export default handleMailTo;
