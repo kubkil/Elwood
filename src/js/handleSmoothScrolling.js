722ea9eace0a49c3f6716e7aceaf13acb84f97f7
Jakub Kilar <kuba.kilar@gmail.com>
2018-11-20 11:06:00 +0100

    Add variable

--- src/js/handleSmoothScrolling.js
+++ src/js/handleSmoothScrolling.js
@@ -9,6 +9,7 @@ const handleSmoothScrolling = (() => {
   const linkToAbout = document.querySelector('.navbar__link--about');
   const linkToContact = document.querySelector('.navbar__link--contact');
   const linkToGallery = document.querySelector('.about__gallery-link');
+  const arrowLink = document.querySelector('.home__link');
   const homeSection = document.querySelector('.home');
   const aboutSection = document.querySelector('.about');
   const gallerySection = document.querySelector('.gallery');
