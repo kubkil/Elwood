dcc5c01ccbfa71adff8e4e4ea799a541a6478e52
Jakub Kilar <kuba.kilar@gmail.com>
2018-11-20 10:20:17 +0100

    Add module
    Module adds onClick attribute with preventDefault to all local links.

---
+++ src/js/handleLinks.js
@@ -0,0 +1,8 @@
+const handleLinks = (() => {
+  const links = document.querySelectorAll('a[href^="#"]');
+  links.forEach(link =>
+    link.setAttribute('onClick', 'event.preventDefault();')
+  );
+})();
+
+export default handleLinks;
