ecce48f4f50bb7188e20baeffaaf57e521d255be
Jakub Kilar <kuba.kilar@gmail.com>
2018-11-20 10:49:44 +0100

    Configure siema Add looping, media queries and changing slides on button click.

--- src/js/handleSiema.js
+++ src/js/handleSiema.js
@@ -1,63 +1,19 @@
 import Siema from 'siema';
 
-if (window.screen.width > 1025) {
-  var mySiema = new Siema({
-    loop: true
-  });
-}
+const mySiema = new Siema({
+  loop: true,
+  perPage: {
+    769: 2,
+    1025: 3
+  }
+});
 
-const handleSiema = (() => {
-  const siemaDiv = document.querySelector('.navbar__container')
-    .nextElementSibling;
-  const linkToHome = [
-    document.querySelector('.navbar__link--home'),
-    document.querySelector('.navbar__logo-link')
-  ];
-  const linkToAbout = document.querySelector('.navbar__link--about');
-  const linkToContact = document.querySelector('.navbar__link--contact');
+const siemaBtns = (() => {
+  const prevBtn = document.querySelector('.gallery__btn--prev');
+  const nextBtn = document.querySelector('.gallery__btn--next');
 
-  const removeSiemaClass = () => {
-    if (siemaDiv.classList.contains('siema')) {
-      siemaDiv.classList.remove('siema');
-    }
-  };
-
-  const addSiemaClass = () => {
-    if (!siemaDiv.classList.contains('siema')) {
-      siemaDiv.classList.add('siema');
-    }
-  };
-
-  const slideOnClick = () => {
-    linkToHome.forEach(link =>
-      link.addEventListener('click', () => mySiema.goTo(0))
-    );
-    linkToAbout.addEventListener('click', () => mySiema.goTo(1));
-    linkToContact.addEventListener('click', () => mySiema.goTo(3));
-  };
-
-  const removeSlideOnClick = () => {
-    linkToHome.forEach(link =>
-      link.removeEventListener('click', () => mySiema.goTo(0))
-    );
-    linkToAbout.removeEventListener('click', () => mySiema.goTo(1));
-    linkToContact.removeEventListener('click', () => mySiema.goTo(3));
-  };
-
-  const media1025 = window.matchMedia('(max-width: 1025px)');
-
-  const siemaMQ = media => {
-    if (media.matches) {
-      removeSlideOnClick();
-      removeSiemaClass();
-    } else {
-      addSiemaClass();
-      slideOnClick();
-    }
-  };
-
-  siemaMQ(media1025);
-  media1025.addListener(siemaMQ);
+  prevBtn.addEventListener('click', () => mySiema.prev());
+  nextBtn.addEventListener('click', () => mySiema.next());
 })();
 
-export default handleSiema;
+export default siemaBtns;
