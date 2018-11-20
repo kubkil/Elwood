af385aa70a72bb00fe5b63331074f8b04046772e
Jakub Kilar <kuba.kilar@gmail.com>
2018-11-20 10:26:12 +0100

    Rebuild module
    Delete match.media and show/hide navbar on scroll instead to making it
    bigger/smaller.

--- src/js/handleNavbarOnScroll.js
+++ src/js/handleNavbarOnScroll.js
@@ -1,51 +1,20 @@
+import debounce from 'lodash.debounce';
+
 const handleNavbarOnScroll = (() => {
   const navbarContainer = document.querySelector('.navbar__container');
   let lastScrollPosition = 0;
 
-  const makeNavbarSmaller = () => {
-    navbar.classList.add('navbar__scrolled-js');
-    elwoodLogo.classList.add('navbar__logo--scrolled-js');
-    navbarLinks.forEach(link =>
-      link.classList.add('navbar__link--scrolled-js')
-    );
-  };
-
-  const makeNavbarBigger = () => {
-    navbar.classList.remove('navbar__scrolled-js');
-    elwoodLogo.classList.remove('navbar__logo--scrolled-js');
-    navbarLinks.forEach(link =>
-      link.classList.remove('navbar__link--scrolled-js')
-    );
-  };
-
-  const setNavbarSize = () => {
-    if (window.pageYOffset > 0) {
-      makeNavbarSmaller();
-      const navbarHeight = navbar.offsetHeight;
-      aboutSection.style.paddingTop = `${navbarHeight + 10}px`;
+  const checkScrollPosition = () => {
+    const newScrollPosition = window.scrollY;
+    if (newScrollPosition > lastScrollPosition) {
+      navbarContainer.classList.add('navbar__container-js-scrolled');
     } else {
-      makeNavbarBigger();
+      navbarContainer.classList.remove('navbar__container-js-scrolled');
     }
-    window.addEventListener('scroll', setNavbarSize);
+    lastScrollPosition = newScrollPosition;
   };
-
-  const media1025 = window.matchMedia('(max-width: 1025px)');
-
-  const scrollMQ = media => {
-    if (media.matches) {
-      console.log('<1025');
-      setNavbarSize();
-    } else {
-      console.log('>1025');
-      makeNavbarBigger();
-      aboutSection.style.removeProperty('padding-top');
-      window.removeEventListener('scroll', setNavbarSize);
-    }
-  };
-
-  scrollMQ(media1025);
-
-  media1025.addListener(scrollMQ);
+  // https://www.yourwebtech.info/tutorial/how-to-detect-vertical-scroll-direction-using-javascript/
+  window.addEventListener('scroll', debounce(checkScrollPosition, 200));
 })();
 
 export default handleNavbarOnScroll;
