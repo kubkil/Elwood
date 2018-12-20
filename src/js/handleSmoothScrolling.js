1d0d104b733195d38933614961a02c6548821f19
Jakub Kilar <kuba.kilar@gmail.com>
2018-12-20 20:26:59 +0100

    Create function that gets all siblings of an element

--- src/js/handleSmoothScrolling.js
+++ src/js/handleSmoothScrolling.js
@@ -42,6 +42,19 @@ const handleSmoothScrolling = (() => {
     '.pagination__button--contact'
   );
 
+  const getAllSiblings = elem => {
+    const siblings = [];
+    let sibling = elem.parentNode.firstChild;
+
+    while (sibling) {
+      if (sibling.nodeType === 1 && sibling !== elem) {
+        siblings.push(sibling);
+      }
+      sibling = sibling.nextSibling;
+    }
+    return siblings;
+  };
+
   const smoothScrollTo = (evt, section) => {
     evt.preventDefault();
     section.scrollIntoView({
