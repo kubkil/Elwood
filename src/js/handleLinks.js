c6bc712079cff3f645a9fb3fa61c57266964ed1f
Jakub Kilar <kuba.kilar@gmail.com>
2019-01-13 14:52:19 +0100

    Delete file

--- src/js/handleLinks.js
+++
@@ -1,8 +0,0 @@
-// const handleLinks = (() => {
-//   const links = document.querySelectorAll('a[href^="#"]');
-//   links.forEach(link =>
-//     link.setAttribute('onClick', 'event.preventDefault();')
-//   );
-// })();
-
-// export default handleLinks;
